0b5a3d35f57380eddcb68abbf09ddfb32a8e0a59
Mudlej <mudlej@proton.me>
2023-02-24 22:38:59 +0300

    Updated native_code.py to look up ndk path.

--- build_dependencies/native_code.py
+++ build_dependencies/native_code.py
@@ -1,14 +1,14 @@
 import os
 from pathlib import Path
 
-from build_dependencies.common import get_lib_path, run_cmd
+from build_dependencies.common import run_cmd, find_ndk_path
 from build_dependencies.values import LIB_DIR_PATH
 
 
 def build_native_code():
     JNI_PATH = Path(f"../{LIB_DIR_PATH}").resolve().parents[0]
     os.chdir(JNI_PATH)
-    run_cmd(["/opt/android-ndk/ndk-build"])
+    run_cmd([f"{find_ndk_path()}/ndk-build"])
 
 
 if __name__ == "__main__":
